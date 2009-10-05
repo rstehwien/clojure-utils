eb70bdca191f5f89f2eae1a989c09437ab08614f
rstehwien <rstehwien@gmail.com>
2009-10-05 08:47:00 -0600

    remove alternate versions of clear-cached-files

--- src/rstehwien/clojure/fileutils.clj
+++ src/rstehwien/clojure/fileutils.clj
@@ -4,28 +4,7 @@
 
 (def file-seq-cache (ref {}))
 
-(defmulti clear-cached-files (fn[& arglist] (count arglist)))
-
-(defmethod clear-cached-files 0 [& arglist]
-  (dosync (alter file-seq-cache empty)))
-
-(defmethod clear-cached-files 1 [& arglist]
-  (dosync (alter file-seq-cache dissoc (first arglist))))
-
-(defmethod clear-cached-files :default [& arglist]
-  (doseq [arg arglist] (clear-cached-files arg)))
-
-(defn clear-cached-files2
-  ([]
-     (dosync (alter file-seq-cache empty)))
-  ([key]
-     (dosync (alter file-seq-cache dissoc key)))
-  ([key & ks]
-     (dosync
-      (clear-cached-files2 key)
-      (doseq [key2 ks] (clear-cached-files key2)))))
-
-(defn clear-cached-files3
+(defn clear-cached-files
   ([]
      (dosync (alter file-seq-cache empty)))
   ([& ks]
