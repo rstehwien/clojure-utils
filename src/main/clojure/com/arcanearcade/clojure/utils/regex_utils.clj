087ab90be7800ac10c23c9f335d42176ebfb7055
rstehwien <rstehwien@gmail.com>
2009-10-11 10:08:45 -0600

    Add commented out doc - metadata without anything else in ns fails compile

--- src/main/clojure/com/arcanearcade/clojure/utils/regex_utils.clj
+++ src/main/clojure/com/arcanearcade/clojure/utils/regex_utils.clj
@@ -1,4 +1,7 @@
-(ns com.arcanearcade.clojure.utils.regex-utils)
+(ns com.arcanearcade.clojure.utils.regex-utils
+;  #^{:author "Robert Stehwien",
+;     :doc "Clojure regular expression utilities"}
+  )
 
 (defn re-match? [re s] (not (nil? (re-matches re (str s)))))
 
